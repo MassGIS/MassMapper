7598d0f38502d87c394e1586cffedc55ad0e2bd1
Saul Farber <saul@peoplegis.com>
2021-05-03 16:22:59 -0400

    added Observer inside the returned function

--- packages/oliver-app/src/components/LegendComponent.tsx
+++ packages/oliver-app/src/components/LegendComponent.tsx
@@ -12,14 +12,14 @@ import {
 	ErrorOutline
 } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
-import { observer } from 'mobx-react';
+import { Observer, observer } from 'mobx-react';
 import React, { FunctionComponent } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { LegendService } from '../services/LegendService';
 import { useService } from '../services/useService';
 import 'leaflet/dist/leaflet.css';
 import { ClassNameMap } from '@material-ui/styles';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 
 interface LegendComponentProps extends RouteComponentProps<any> {
 }
@@ -38,105 +38,106 @@ const LegendComponent: FunctionComponent<LegendComponentProps> = observer(({}) =
 	const [ legendService ] = useService([ LegendService ]);
 	const classes = useStyles();
 
-	const onDragEnd = result => {
-		console.dir(result);
-	}
-
 	return (
-		<DragDropContext 
-			onDragEnd={onDragEnd} 
+		<DragDropContext
+			onDragEnd={(result) => {
+				console.dir(result);
+			}}
 		>
 			<FormControl className={classes.formControl} component="fieldset">
 				<Droppable droppableId="foo">
 					{(provided) => (
-							<FormGroup
-								ref={provided.innerRef}
-								{...provided.droppableProps}
-							>
-								{legendService.layers.map((l, i) => {
-									console.log(i);
-									// Don't show a legend image if we have none.
-									const img = l.legendURL ? (
-										<img
-											src={l.legendURL}
-											className='img-fluid'
-											alt={l.name}
-										/>
-									) : '';
+							<Observer>{(): JSX.Element => {
+								return (<FormGroup
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+								>
+									{legendService.layers.map((l, i) => {
+										console.log(i);
+										// Don't show a legend image if we have none.
+										const img = l.legendURL ? (
+											<img
+												src={l.legendURL}
+												className='img-fluid'
+												alt={l.name}
+											/>
+										) : '';
 
-									let status = <span/>;
-									let image = <span/>;
-									if (l.enabled) {
-										if (l.scaleOk) {
-											status = l.isLoading ? <CircularProgress size="20px"/> : status;
-											image = l.legendURL ? (
-												<img
-													src={l.legendURL}
-													className='img-fluid'
-													alt={l.name}
-													style={{maxWidth: 200}}
-												/>
-											) : image;
+										let status = <span/>;
+										let image = <span/>;
+										if (l.enabled) {
+											if (l.scaleOk) {
+												status = l.isLoading ? <CircularProgress size="20px"/> : status;
+												image = l.legendURL ? (
+													<img
+														src={l.legendURL}
+														className='img-fluid'
+														alt={l.name}
+														style={{maxWidth: 200}}
+													/>
+												) : image;
+											}
+											else {
+												status =
+													<Tooltip title="Out of scale">
+														<ErrorOutline fontSize="small"/>
+													</Tooltip>;
+											}
 										}
-										else {
-											status =
-												<Tooltip title="Out of scale">
-													<ErrorOutline fontSize="small"/>
-												</Tooltip>;
-										}
-									}
-									
-									return (
-										<Draggable key={l.id} draggableId={l.id} index={i}>
-											{(provided) => (
-												<FormControlLabel
-													{...provided.draggableProps}
-													{...provided.dragHandleProps}
-													ref={provided.innerRef}
-													style={{display: 'table'}}
-													control={
-														<div style={{display: 'table-cell', width: 55}}>
-															<Tooltip
-																title="enable/disable layer"
-															>
-																<Checkbox
-																	className={classes.button}
-																	onChange={(e) => {
-																		l.enabled = e.target.checked;
-																	}}
-																	checked={l.enabled}
-																	color="default"
-																/>
-															</Tooltip>
-															<Tooltip
-																title="remove layer"
-															>
-																<IconButton
-																	className={classes.button}
-																	onClick={() => {
-																		legendService.removeLayer(l);
-																	}}
+
+										return (
+											<Draggable key={l.id} draggableId={l.id} index={i}>
+												{(provided) => (
+													<FormControlLabel
+														{...provided.draggableProps}
+														{...provided.dragHandleProps}
+														ref={provided.innerRef}
+														style={{display: 'table'}}
+														control={
+															<div style={{display: 'table-cell', width: 55}}>
+																<Tooltip
+																	title="enable/disable layer"
 																>
-																	<DeleteOutline />
-																</IconButton>
-															</Tooltip>
-														</div>
-													}
-													key={`layer-${l.id}`}
-													label={
-														<div style={{whiteSpace: 'nowrap' }}>
-															{l.title}&nbsp;&nbsp;{status}<br/>
-															{image}
-														</div>
-													}
-												/>
-											)}
-										</Draggable>
-									);
-								})}
-								{provided.placeholder}
-							</FormGroup>
-						
+																	<Checkbox
+																		className={classes.button}
+																		onChange={(e) => {
+																			l.enabled = e.target.checked;
+																		}}
+																		checked={l.enabled}
+																		color="default"
+																	/>
+																</Tooltip>
+																<Tooltip
+																	title="remove layer"
+																>
+																	<IconButton
+																		className={classes.button}
+																		onClick={() => {
+																			legendService.removeLayer(l);
+																		}}
+																	>
+																		<DeleteOutline />
+																	</IconButton>
+																</Tooltip>
+															</div>
+														}
+														key={`layer-${l.id}`}
+														label={
+															<div style={{whiteSpace: 'nowrap' }}>
+																{l.title}&nbsp;&nbsp;{status}<br/>
+																{image}
+															</div>
+														}
+													/>
+												)}
+											</Draggable>
+										);
+									})}
+									{provided.placeholder}
+								</FormGroup>)
+
+							}}</Observer>
+
 					)}
 				</Droppable>
 			</FormControl>
