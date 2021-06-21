3d931849fe69f2bf0c2e20feac3fc50ffc5186ae
Saul Farber <saul@peoplegis.com>
2021-06-21 19:20:07 -0400

    support coordinates in different projections

--- packages/massmapper-app/src/components/ShowCoordinatesToolComponent.tsx
+++ packages/massmapper-app/src/components/ShowCoordinatesToolComponent.tsx
@@ -1,18 +1,28 @@
 import React, { FunctionComponent } from "react";
 import { ToolComponentProps } from "../models/Tool";
-import { ShowCoordinatesTool } from "../models/ShowCoordinatesTool";
+import { ShowCoordinatesTool, units } from "../models/ShowCoordinatesTool";
 import { observer } from "mobx-react-lite";
-import { Paper } from "@material-ui/core";
+import {
+	Button,
+	Menu,
+	MenuItem,
+	Paper,
+	Typography,
+} from "@material-ui/core";
+import { ArrowDropDown } from "@material-ui/icons";
 
 const ShowCoordinatesToolComponent: FunctionComponent<ToolComponentProps> = observer(({tool}) => {
 	const myTool = tool as ShowCoordinatesTool;
+	const handleClose = (e:any) => {
+		myTool.isChangingUnits = false;
+		myTool.units = e.target.getAttribute('value')
+	}
 	return (
 		<Paper
 			style={{
 				height:'80%',
 				padding: '0 1em',
 				position: 'relative',
-				top: '5px'
 			}}
 			elevation={3}
 		>
@@ -21,7 +31,46 @@ const ShowCoordinatesToolComponent: FunctionComponent<ToolComponentProps> = obse
 				top: '11px',
 				fontWeight: 'bold'
 			}}>
-				lat: {myTool.xCoord} &nbsp; lon:{myTool.yCoord}
+				<Button
+					style={{
+						marginTop: '0',
+						paddingTop: '0',
+					}}
+					ref={(r:HTMLButtonElement) => {
+						myTool.buttonRef = r;
+					}}
+					onClick={() => {
+						myTool.isChangingUnits = true;
+					}}
+					>
+					{myTool.xCoord}
+					,&nbsp;
+					{myTool.yCoord}
+					&nbsp;&nbsp;&nbsp;&nbsp;
+					<Typography
+						variant="caption"
+					>
+						{myTool.units}
+					</Typography>
+					<ArrowDropDown />
+				</Button>
+				<Menu
+					keepMounted
+					open={myTool.isChangingUnits}
+					onClose={() => {
+						myTool.isChangingUnits = false;
+					}}
+					anchorEl={myTool.buttonRef}
+					style={{
+						// width: '4em'
+					}}
+					// variant="standard"
+				>
+					<MenuItem onClick={handleClose} value={units.DMS}>lat/lon (dms)</MenuItem>
+					<MenuItem onClick={handleClose} value={units.DD}>lat/lon (dd)</MenuItem>
+					<MenuItem onClick={handleClose} value={units.SP_METERS}>SP Meters</MenuItem>
+					<MenuItem onClick={handleClose} value={units.SP_FEET}>SP Feet</MenuItem>
+				</Menu>
 			</div>
 		</Paper>
 	)
