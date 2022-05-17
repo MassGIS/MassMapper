bcbae58fe0aefd472cc0b21306086fb830917579
Charlton Galvarino <charlton@2creek.com>
2022-05-17 15:14:25 -0700

    put arcgis geocoder tool input directly on map

--- packages/massmapper-app/src/components/ArcGISGeocodeToolComponent.tsx
+++ packages/massmapper-app/src/components/ArcGISGeocodeToolComponent.tsx
@@ -1,27 +1,16 @@
 import XMLParser from 'react-xml-parser';
 import {
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogTitle,
-	Grid,
 	TextField,
-} from '@material-ui/core';
-import {
-	Close,
-	Explore,
-	ImageSearch,
-	Search,
-} from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
+	Paper,
+} from '@material-ui/core'
 import { latLng } from 'leaflet';
-import { observer, useLocalObservable } from 'mobx-react-lite';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { autorun } from 'mobx';
+import { Observer, useLocalObservable } from 'mobx-react-lite';
 import React, { FunctionComponent } from "react";
 import { MapService } from '../services/MapService';
 import { useService } from '../services/useService';
-import { toast } from 'react-toastify';
-import { Tool, ToolComponentProps } from '../models/Tool';
+
 
 interface ArcGISGecodeResult {
 	address: string;
@@ -30,9 +19,7 @@ interface ArcGISGecodeResult {
 		y: number,
 	}
 };
-
 const arcgisGeocode = async(addr:string):Promise<Array<ArcGISGecodeResult>> => {
-
 	//method  : "POST"
 	//headers : {'Content-Type':'text/xml; charset=UTF-8'}
 	const body = `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
@@ -94,201 +81,126 @@ const arcgisGeocode = async(addr:string):Promise<Array<ArcGISGecodeResult>> => {
 	});
 }
 
-const useStyles = makeStyles((theme) => ({
-	paper: {
-		'& .MuiPaper-root': {
-			height: '35vh',
-			width: '90vw',
-			pointerEvents: 'auto'
-		},
-		'&' : {
-			pointerEvents: 'none'
-		}
-	},
-}));
-
-interface ArcGISGeocodeToolComponentState {
-	street: string;
-	results: Array<ArcGISGecodeResult>;
-	tool: Tool;
+interface ArcGISGeocodeToolComponentProps {
 }
 
-const ArcGISGeocodeToolComponent: FunctionComponent<ToolComponentProps> = observer(({tool}) => {
+interface ArcGISGeocodeToolComponentState {
+	isFocused: boolean;
+	searchString: string;
+	searchResults: ArcGISGecodeResult[];
+	isSearchLoading: boolean;
+	noOptionsText: string;
+	inputValue: string;
+}
+
+const ArcGISGeocodeToolComponent: FunctionComponent<ArcGISGeocodeToolComponentProps> = () => {
 	const myState = useLocalObservable<ArcGISGeocodeToolComponentState>(() => {
 		return {
-			street: '',
-			results: [],
-			tool,
+			isFocused: false,
+			searchString: '',
+			searchResults: [],
+			isSearchLoading: false,
+			noOptionsText: '',
+			inputValue: '',
 		}
 	});
 
-	const classes = useStyles();
+	const [mapService] = useService([MapService]);
+
+	autorun(async () => {
+		if (!myState.searchString || myState.searchString.length < 3) {
+			return;
+		}
+
+		const res = await arcgisGeocode(myState.searchString);
+		myState.isSearchLoading = false;
+		if (res.length === 0) {
+			// no results
+			myState.noOptionsText = 'No matches found';
+		}
+
+		myState.searchResults = res;
+	}, {
+		delay: 400
+	});
 
 	return (
-		<>
-			<Button
-				style={{
-					minWidth: '32px',
-					backgroundColor: tool.isActive ? '' : 'white',
-				}}
-				color="default"
-				variant="contained"
-				size="small"
-				title="Search and zoom to an address"
-				onClick={() => {
-					tool.activate();
-				}}
+		<Observer>{() =>
+			(<Paper
+				elevation={2}
 			>
-				<ImageSearch />
-			</Button>
-			{tool.isActive && (
-				<Dialog
-					open
-					className={classes.paper}
-					onClose={() => {
-						myState.street = '';
-						myState.results = [];
-						tool.deactivate();
-					}}
-					BackdropProps={{
-						invisible: true
-					}}
-				>
-					<DialogActions>
-						<Button
-							size="small"
-							style={{
-								float: 'right'
-							}}
-							onClick={() => {
-								myState.street = '';
-								myState.results = [];
-								tool.deactivate();
-							}}
-						>
-							<Close />
-						</Button>
-					</DialogActions>
-					<DialogTitle>
-						{myState.results.length > 0 ? 'Search Results' : 'Location Search'}
-					</DialogTitle>
-					<DialogContent
-						style={{
-							overflowY: 'hidden'
-						}}
-					>
-						<Grid
-							style={{
-								flexGrow: 1,
-								height: '40vh',
-							}}
-						>
-							{myState.results.length > 0 && (<ResultsComponent uiState={myState} />)}
-							{myState.results.length === 0 && (<SearchComponent uiState={myState} />)}
-						</Grid>
-					</DialogContent>
-				</Dialog>
-			)}
-		</>
-	);
-});
-
-const SearchComponent: FunctionComponent<{uiState: ArcGISGeocodeToolComponentState}> = observer(({uiState}) => {
-	return (
-		<Grid
-			style={{
-				height: '85%',
-				marginLeft: '2em'
-			}}
-		>
-			<TextField
-				variant="outlined"
-				style={{
-					width:'80%',
-					marginBottom:'.5em',
-				}}
-				value={uiState.street}
-				placeholder="Find address or place"
-				onChange={(e) => {
-					uiState.street = e.target.value;
-				}}
-			/>
-			<br/>
-			<Button
-				onClick={async () => {
-					uiState.results = await arcgisGeocode(uiState.street);
-					if (uiState.results.length === 0) {
-						toast("No matches found.  Please try again.");
-					}
-				}}
-				disabled={!uiState.street}
-				variant="contained"
-			>
-				<Search /> Search
-			</Button>
-		</Grid>
-	)
-});
-
-
-const ResultsComponent: FunctionComponent<{uiState: ArcGISGeocodeToolComponentState}> = observer(({uiState}) => {
-	const mapService = useService(MapService);
-	return (
-		<>
-			<Grid
-				container
-				direction="row"
-				style={{
-					height: '85%',
-					padding: '0 2em',
-				}}
-			>
-				<Grid
-					container
-					direction="row"
+				<div
 					style={{
-						marginBottom: '1em'
+						height: '44px',
+						width: myState.isFocused ? '40em' : '15em',
+						backgroundColor: 'white',
+						padding: '.4em 1em',
+						borderRadius: '3px',
 					}}
 				>
-					{uiState.results.map((r) => {
-						return (
-							<Grid
-								item
-								style={{
-									height: '1.4em'
-								}}
-								key={Math.random()}
-							>
-								<Button
-									onClick={async () => {
-										mapService.leafletMap?.setView(latLng(r.location.y, r.location.x), 19);
-										uiState.street = '';
-										uiState.results = [];
-										uiState.tool.deactivate()
-									}}
-									variant="contained"
-								>
-									<Explore />
-								</Button>
-								&nbsp;&nbsp;
-								{r.address}
-							</Grid>
-						)
-					})}
-				</Grid>
-				{/* <Grid item>
-					<Button
-						onClick={async () => {
-							uiState.results = [];
+					<Autocomplete
+						loading={ myState.isSearchLoading }
+						clearOnEscape
+						clearOnBlur
+						options={ myState.searchResults }
+						noOptionsText={myState.noOptionsText}
+						getOptionLabel={(o:ArcGISGecodeResult) => { return o.address || ''}}
+						inputValue={myState.inputValue}
+						onFocus={() => {
+							myState.isFocused = true;
+							window.setTimeout(() => {
+								myState.noOptionsText = 'Enter address above...';
+							}, 10);
 						}}
-						variant="contained"
-					>
-						<ArrowBack /> Back
-					</Button>
-				</Grid> */}
-			</Grid>
-		</>
-	)
-});
+						onInputChange={(e, val) => {
+							myState.inputValue = val;
+						}}
+						onBlur={(e) => {
+							myState.isFocused = false;
+							myState.isSearchLoading = false;
+							myState.noOptionsText = '';
+							myState.searchResults = [];
+							myState.inputValue = '';
+						}}
+						onKeyUp={(e) => {
+							const val = (e.target as HTMLInputElement).value;
+							if (/enter/i.test(e.code)) {
+								myState.isSearchLoading = true;
+								myState.searchString = val;
+							}
+							else if (!/arrow/i.test(e.code)) {
+								myState.isSearchLoading = false;
+								myState.searchResults = [];
+							}
+						}}
+						filterOptions={(o) => {
+							return o;
+						}}
+						// onchange only fires on actually *selecting* an option
+						onChange={(e,v:any) => {
+							if (!v) {
+								myState.searchResults = [];
+								myState.searchString = '';
+								return;
+							}
+							mapService.leafletMap?.setView(latLng(v.location.y, v.location.x), 19);
+						}}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								style={{
+									width: '100%'
+								}}
+								placeholder={myState.isFocused ? '' : 'Enter a location...'}
+							/>
+						)}
+					/>
+
+				</div>
+			</Paper>)
+		}</Observer>
+
+	);
+};
 
 export { ArcGISGeocodeToolComponent }
