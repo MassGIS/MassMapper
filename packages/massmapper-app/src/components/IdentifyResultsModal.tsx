e593f953f04b00e037dae5ac3a74ac94023cee0d
Charlton Galvarino <charlton@2creek.com>
2025-05-23 08:14:24 -0400

    use scroll bars on right... (#85)

--- packages/massmapper-app/src/components/IdentifyResultsModal.tsx
+++ packages/massmapper-app/src/components/IdentifyResultsModal.tsx
@@ -187,7 +187,7 @@ const IdentifyResultsModal: FunctionComponent<IdentifyResultsModalProps> = obser
 				}}
 				id="identify-dialog-title"
 			>
-				Identify Results
+				Identify Results {myState.windowSize === 'xl' ? '(use scroll bars on right to see all layers/features)' : ''}
 				<div style={{float: 'right'}}>
 					<Button
 						size="small"
